1d9b98c998a2eccafef34b39caf04596a378f725
Melanie <melanie.legigan@student.junia.com>
2023-01-10 14:26:18 +0100

    Adding content to Events + news

--- client/src/Views/Events.js
+++ client/src/Views/Events.js
@@ -9,7 +9,38 @@ function Events(){
             title = "Ce qu’il se passe autour"
             subtitle = "Retrouvez ci-dessous tous les événements autour de vous"
         >
-            <ArticleItem></ArticleItem>
+            <ArticleItem
+                date = "November 21, 2022"
+                content = "La ferme d’Hugo et ses fils accueille enfants comme adultes lors d’une journée de partage le 21 Novembre. Venez découvrir ses animaux et sa culture de céréales. Un échange autour d’une tartine de beurre est même.."
+                title = "Visite d’une ferme pédagogique à Ennetières"
+                url = "image 1.png"
+                link = "#"
+                textButton = "Je suis interessé"
+            ></ArticleItem>
+            <ArticleItem
+                date = "November 17, 2022"
+                content = "Noël approche, venez décorer des sapins de Noël avec les fruits de la ferme de Francine et Alexandre! Ouverte de 13h30 à 17h30 le 17 Novembre. 8 places disponibles !"
+                title = "Décoration des fruits de Noël"
+                url = "image 2.png"
+                link = "#"
+                textButton = "Je suis interessé"
+            ></ArticleItem>
+            <ArticleItem
+                date = "September 27, 2022"
+                content = "Découvrez toutes les ventes locales de la semaine dans cet articles vous présentant les fermes et leurs produits! Rien de mieux pour commencer la rentrée."
+                title = "Ventes de la semaine"
+                url = "image 3.png"
+                link = "#"
+                textButton = "Je suis interessé"
+            ></ArticleItem>
+            <ArticleItem
+                date = "July 7, 2022"
+                content = "Découvrez l’ensemble des marchés prévus dans la région pour ce deuxième semestre de 2022. Planifiez au mieux vos achats en direct aux producteurs et découvrez les produits de saison. "
+                title = "Marchés de région - 2 ème semestre 2022"
+                url = "image 4.png"
+                link = "#"
+                textButton = "Je suis interessé"
+            ></ArticleItem>
         </Articles>
     );
 }
--- client/src/Views/News.js
+++ client/src/Views/News.js
@@ -9,7 +9,38 @@ function News(){
             title = "Découvrez nos actualités"
             subtitle = "Retrouvez ci-dessous toutes l'actualités et les articles publiés par l'équipe d'Alvie."
         >
-            <ArticleItem></ArticleItem>
+            <ArticleItem
+                date = "December 31, 2022"
+                content = "Près de 70 % des exploitants agricoles utilisent des adjuvants¹. Ce chiffre monte à presque 100 % pour les agriculteurs céréaliers. Leur usage s'est démocratisé au fil des années et ils sont aujourd’hui devenus presque incontournables. Mais quel est le rôle réel de l’adjuvantation ? Comment bien choisir son adjuvantation ? Est-ce qu’une bonne adjuvantation maximise les effets du traitement appliqué ?"
+                title = "Les secrets des adjuvants révélés : comment maximiser vos traitements grâce à l’adjuvantation ?"
+                url = "22_12_31.png"
+                link = "#"
+                textButton = "Lire l'article"
+            ></ArticleItem>
+            <ArticleItem
+                date = "November 21, 2022"
+                content = "Vous avez besoin de repasser au plus vite le test ? Est-il impératif pour votre activité d’obtenir le certiphyto ? Retrouvez ci-dessous les 5 meilleurs conseils à mettre en pratique pour décrocher votre Certiphyto rapidement."
+                title = "Le top 5 des conseils pour réussir le certiphyto"
+                url = "22_11_21.jpg"
+                link = "#"
+                textButton = "Lire l'article"
+            ></ArticleItem>
+            <ArticleItem
+                date = "September 27, 2022"
+                content = "Il est courant de moduler ses doses de fongicides (en fonction de la météo, de la pression maladie…). Ce qui l’est moins c’est de moduler les herbicides. Gérer les adventices dans ses parcelles demande une grande technicité, un désherbage mal géré une année peut engendrer des difficultés sur plusieurs campagnes suivantes."
+                title = "Moduler ses herbicides"
+                url = "22_09_27.png"
+                link = "#"
+                textButton = "Lire l'article"
+            ></ArticleItem>
+            <ArticleItem
+                date = "May 5, 2022"
+                content = "Une météo non optimale avec une moyenne de 15 km/h de vent et un temps sec rendent les traitements à base d’herbicides racinaires compliqués pour les agriculteurs des Hauts-de-France. Plusieurs stratégies pour réussir son désherbage dans ces conditions sont possibles."
+                title = "ACTUS : Traitement : les conditions météos difficiles dans les Hauts-de-France nécessitent d’adapter la stratégie de pulvérisation pour réussir le désherbage de la betterave."
+                url = "22_05_25.webp"
+                link = "#"
+                textButton = "Lire l'article"
+            ></ArticleItem>
         </Articles>
     );
 }
