a0123ccf39ab5f721f3783c63b150fcbd07c4472
py2378 <colasbou@gmail.com>
2023-01-13 11:48:27 +0100

    add toggle legend using cookies

--- client/src/Components/Map/Map.js
+++ client/src/Components/Map/Map.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MapContainer, TileLayer,Marker,Popup,Polygon} from 'react-leaflet';
 import {Icon} from "leaflet";
+import { useCookies } from 'react-cookie'
 import locationList from "../../Data/location.json";
 import farmList from "../../Data/farm.json";
 import weatherZoneList from "../../Data/weatherZone.json";
@@ -16,6 +17,8 @@ const locationIcon = new Icon({
 })
 
 const Map = () => {
+    const [cookies, setCookie] = useCookies()
+    console.log(cookies.local);
     function getColorWeather(weather) {
         switch (weather) {
             case "clear":
@@ -47,6 +50,7 @@ const Map = () => {
         }
         return objList;
     };
+
     const objList = convertArrayToObject(weatherZoneList.features);
     filteredLocation(locationList.features);
 
@@ -58,19 +62,19 @@ const Map = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {farmList.features.map(farm => (
+            {((cookies.local)? cookies.local ==='true':true)  && farmList.features.map(farm => (
                 <Marker icon={farmIcon} key={farm.properties.id} position={[farm.geometry.coordinates[1], farm.geometry.coordinates[0]]}>
                 <Popup>
                     {farm.properties.name} <br /> <a href={farm.properties.website}>site internet</a>
                 </Popup>
             </Marker>))};
-            {locationList.features.map(location => (
+            {((cookies.local)? cookies.local ==='true':true)  && locationList.features.map(location => (
                 <Marker icon={locationIcon} key={location.properties.id_pdv} position={[location.properties.geo[0], location.properties.geo[1]]}>
                     <Popup>
                         {location.properties.libelle_point_vente} <br /> {location.properties.societe_producteur}
                     </Popup>
                 </Marker>))};
-            {weatherZoneList.features.map(weatherZone => (
+            {((cookies.weather)? cookies.weather ==='true':true) && weatherZoneList.features.map(weatherZone => (
                 <Polygon key = {weatherZone.properties.id} fillOpacity='0.85' color = {getColorWeather(weatherZone.properties.weather)}  positions={objList[weatherZone.properties.id]} >
                 </Polygon>)
             )}
--- client/src/Components/SideBar/Filter.js
+++ client/src/Components/SideBar/Filter.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react';
+import { useCookies } from 'react-cookie'
 function Filter(){
-    const [checkedLocal, setCheckedLocal] = useState(true);
-    const [checkedWeather, setCheckedWeather] = useState(true);
+
+    const [cookies, setCookie] = useCookies();
+    const [checkedLocal, setCheckedLocal] = useState((cookies.local)? cookies.local ==='true':true)
+    const [checkedWeather, setCheckedWeather] = useState((cookies.weather)? cookies.weather ==='true':true);
     function toggleLocal(){
         setCheckedLocal(!checkedLocal);
+        let expires = new Date();
+        expires.setTime(expires.getTime() + ( 24*60*60 * 1000))
+        setCookie('local', !checkedLocal , { path: '/',  expires})
+
     }
     function toggleWeatherZone(){
         setCheckedWeather(!checkedWeather);
+        let expires = new Date();
+        expires.setTime(expires.getTime() + ( 24*60*60 * 1000))
+        setCookie('weather', !checkedWeather , { path: '/',  expires})
     }
     return(
         <ul id="sidebar-filter">
